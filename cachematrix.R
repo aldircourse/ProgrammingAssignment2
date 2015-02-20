0b4d5da3d3883ab8b89debee3803071fc4adbff4
miguel angel baños <aldir2001@gmail.com>
2015-02-20 16:38:22 +0100

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inverseMatrix<-NULL
+      set <- function(y) {
+            ## assign external matrix to inner object
+            x <<- y
+            ## as we have a new matrix, old inverse matrix is not valid
+            inverseMatrix <<- NULL
+      }
+      
+      get <- function(){
+            ##return value of x
+            x
+      }
+      
+      setInverse <- function(invMatrix){ 
+            inverseMatrix<<- invMatrix
+      }
+      
+      getInverse <- function(){
+            inverseMatrix
+      } 
+      
+      list(set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
 
@@ -12,4 +35,30 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      iMatrix <- x$getInverse()
+      if(!is.null(iMatrix)) {
+            message("getting cached data")
+            return(iMatrix)
+      }
+      data <- x$get()
+      iMatrix <- solve(data, ...)
+      x$setInverse(iMatrix)
+      iMatrix
 }
+
+useInverse<-function(matrix, ...){
+      initTime<-Sys.time()
+      v<-makeCacheMatrix(matrix)
+      m1<-cacheSolve(v)
+      midTime<-Sys.time()
+      m2<-cacheSolve(v)
+      endTime<-Sys.time()
+      
+      #perfomance statistics
+      noCacheTime<-diff.POSIXt(c(initTime,midTime))
+      cacheTime<-diff.POSIXt(c(midTime,endTime))
+      
+      print(paste("noCache: ",noCacheTime))
+      print(paste("cacheTime: ",cacheTime))
+}
+
